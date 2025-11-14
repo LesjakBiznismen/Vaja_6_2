172446c2cb3dbfea65da72e8f1bd0b61b258bf4c
Miha <miha.mrzdovnik.20639@dijak.sc-celje.si>
2025-11-14 10:09:09 +0100

    dodal vpis stevilk

--- ConsoleApp1/pravokotnik.cs
+++ ConsoleApp1/pravokotnik.cs
@@ -16,5 +16,13 @@ namespace ConsoleApp1
             
             b = 1;
         }
+
+        public pravokotnik(double a, double b)
+        {
+            Console.WriteLine("Vpiši št. a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vpiši št. b: ");
+            b = double.Parse(Console.ReadLine());
+        }
     }
 }
