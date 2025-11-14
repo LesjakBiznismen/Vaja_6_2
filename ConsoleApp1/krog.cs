12c2dee6309a0f12ee33d1648c0c2aabb7b29c55
jakakrajsek <jaka.krajsek@gmail.com>
2025-11-14 09:58:37 +0100

    radius 1

---
+++ ConsoleApp1/krog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class krog
+    {
+        private double r;
+        public krog()
+        {
+            r = 1;
+        }
+
+        public krog(double r)
+        {
+            this.r = r;
+        }
+
+        public void NastaviR()
+        {
+            Console.WriteLine("Vnesi velikost R:");
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double Obseg()
+        {
+            return 2 * Math.PI * r;
+        }
+
+        public double Ploscina()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
+
